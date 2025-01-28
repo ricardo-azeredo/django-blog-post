ae64dd1eaec1fc73456c84ce6cc40cee50cc6efd
Ricardo <ricoazeredo@yahoo.com.br>
2025-01-28 11:47:58 -0300

    update: add template global

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from post.views import post_list
+from post.views import post_list, contact, about
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', post_list),    
+    path('about/', about),    
+    path('contact/',contact),    
 ]
--- blog/post/views.py
+++ blog/post/views.py
@@ -6,3 +6,9 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, "post_list.html", {"posts": posts })
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
